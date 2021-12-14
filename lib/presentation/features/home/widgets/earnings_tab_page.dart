79b7b59aae80963d1cf6081be3a714c8f8b2ccf2
Byron Kimani <byronkimani@gmail.com>
2021-12-14 10:21:07 +0300

    feat: add home page tab bar view
    - add home page tab bar view
    
    Signed-off-by: Byron Kimani <byronkimani@gmail.com>

---
+++ lib/presentation/features/home/widgets/earnings_tab_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EarningTabPage extends StatelessWidget {
+  const EarningTabPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Earnings'),
+    );
+  }
+}
---
+++ lib/presentation/features/home/widgets/rating_tab_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RatingTabPage extends StatelessWidget {
+  const RatingTabPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Ratings'),
+    );
+  }
+}
